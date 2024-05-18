e6d4704576e4718172a27b0b9833358c78e580c2
italomsilva <italoroyale13@gmail.com>
2024-05-18 18:17:27 -0300

    refactor: reorganize page structure

--- menu-mobile.js
+++
@@ -1,7 +0,0 @@
-const MenuBurguer = document.querySelector('.menu-burguer');
-const ListaMenu = document.querySelector('.menu-lista');
-
-MenuBurguer.addEventListener('click', () =>{
-    MenuBurguer.classList.toggle('ativo');
-    ListaMenu.classList.toggle('ativo');
-})
