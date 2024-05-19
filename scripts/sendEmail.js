31c7cdde0b646bced7e17a6ff00fa360cbaacb5b
italomsilva <italoroyale13@gmail.com>
2024-05-19 16:38:01 -0300

    update

--- scripts/sendEmail.js
+++ scripts/sendEmail.js
@@ -3,7 +3,7 @@ function sendEmail() {
   var email = document.getElementById("iemail").value;
   var msg = document.getElementById("imsg").value;
 
-    var mailtoLink = 'mailto:italoroyale13@gmail.com?subject='+encodeURIComponent(`Contact from Portfólio: ${nome}`)+'&body=';
+    var mailtoLink = 'mailto:italoroyale13@gmail.com?subject='+encodeURIComponent(`Portfólio Contact: ${nome}`)+'&body=';
     var layoutMsg = encodeURIComponent(`Olá meu nome é ${nome}\n\n\n ${msg}\n\n Espero Resposta pelo email: ${email}\n\n Atenciosamente, \n ${nome}`);
     var fullUrl = mailtoLink+layoutMsg
     window.location.href = fullUrl;
