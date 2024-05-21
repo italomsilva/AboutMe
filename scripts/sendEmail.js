aed90608937c557981b7b8cfa637e78902564936
italomsilva <italoroyale13@gmail.com>
2024-05-21 09:48:56 -0300

    update

--- scripts/sendEmail.js
+++ scripts/sendEmail.js
@@ -3,8 +3,8 @@ function sendEmail() {
   var email = document.getElementById("iemail").value;
   var msg = document.getElementById("imsg").value;
 
-    var mailtoLink = 'mailto:italoroyale13@gmail.com?subject='+encodeURIComponent(`Portfólio Contact: ${nome}`)+'&body=';
-    var layoutMsg = encodeURIComponent(`Olá meu nome é ${nome}\n\n\n ${msg}\n\n Espero Resposta pelo email: ${email}\n\n Atenciosamente, \n ${nome}`);
+    var mailtoLink = 'mailto:dev.italoms@gmail.com?subject='+encodeURIComponent(`Portfólio Contact: ${nome}`)+'&body=';
+    var layoutMsg = encodeURIComponent(`Olá meu nome é ${nome}\n\n\n ${msg}\n\n Espero Resposta pelo email: ${email}\n\n Atenciosamente, ${nome}`);
     var fullUrl = mailtoLink+layoutMsg
     window.location.href = fullUrl;
   return true;
